f5ed77e53491aa45e13a9261595549cf608303bb
Jinze Xin <jinzexin@Jinzes-MBP.attlocal.net>
2023-10-30 23:59:39 -0700

    Added postBuild for Binder configuration

--- postBuild.sh
+++ postBuild.sh
@@ -3,5 +3,7 @@
 # Install Dash
 pip install dash==1.20.0
 
+jupyter labextension install @plotly/dash-jupyterlab
+
 # Allow Binder to run the Dash app on port 8050
 echo 'c.ServerProxy.servers = {"8050": {"command": ["python", "simulation_script.py"]}}' >> ~/.jupyter/jupyter_notebook_config.py
