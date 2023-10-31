cf1996e6e381671b97dbc7337077a74bcdf7fd8f
Jinze Xin <jinzexin@Jinzes-MBP.attlocal.net>
2023-10-31 00:05:19 -0700

    add a few dependencies

--- postBuild.sh
+++ postBuild.sh
@@ -4,6 +4,8 @@
 pip install dash==1.20.0
 
 jupyter labextension install @plotly/dash-jupyterlab
+jupyter lab build
+
 
 # Allow Binder to run the Dash app on port 8050
 echo 'c.ServerProxy.servers = {"8050": {"command": ["python", "simulation_script.py"]}}' >> ~/.jupyter/jupyter_notebook_config.py
